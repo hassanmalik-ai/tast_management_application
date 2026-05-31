cc9897d72d73d6951b8606d470487d134ccf96a4
HassanMalik-AI <hassanmalik1437@gmail.com>
2026-06-01 01:50:34 +0500

    Update data fetch from id

--- src/task/controller.py
+++ src/task/controller.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from src.task.model import Task
 from fastapi import Depends
 from src.utils.db import get_db
+from fastapi import HTTPException
 
 
 def create_task(body:TaskSchema, db: Session = Depends(get_db)):
@@ -22,4 +23,10 @@ def create_task(body:TaskSchema, db: Session = Depends(get_db)):
 def get_all_task(db: Session = Depends(get_db)):
     tasks = db.query(Task).all()
     return {"tasks": tasks}
+
+def get_one_task(id:int,db:Session = Depends(get_db)):
+    task = db.query(Task).filter(Task.id == id).first()
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return {"task": task}
 
--- src/task/routers.py
+++ src/task/routers.py
@@ -16,3 +16,8 @@ def get_all_task(db = Depends(get_db)):
     return controller.get_all_task(db)
 
 
+@router.get('/get-one/{id}')
+def get_one_task(id:int,db = Depends(get_db)):
+    return controller.get_one_task(id,db)
+
+
